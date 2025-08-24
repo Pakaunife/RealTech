a28295c38559954593ca39cf94a2ce9ac859694d
RENZO <renzo.albertini@edu.unife.it>
2025-08-24 18:12:23 +0200

    Pagine inizali
    footer,header e collegamenti iniziali

--- Cinema/src/app/app.routes.ts
+++ Cinema/src/app/app.routes.ts
@@ -1,3 +1,9 @@
-import { Routes } from '@angular/router';
+import { About } from './pagine/about/about';
+import { Home } from './pagine/home/home';
 
-export const routes: Routes = [];
+
+export const routes = [
+  { path: '', component: Home },
+  { path: 'home', component: Home },
+  { path: 'about', component: About }
+];
