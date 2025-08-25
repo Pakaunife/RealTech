117044adef8bb620bccd41fbf2a2e2016747266a
RENZO <renzo.albertini@edu.unife.it>
2025-08-25 18:08:32 +0200

    home

--- Cinema/src/app/pagine/home/home.ts
+++ Cinema/src/app/pagine/home/home.ts
@@ -1,13 +1,30 @@
 import { Component } from '@angular/core';
 import { Header } from '../../header/header';
 import { Footer } from '../../footer/footer';
+import { CommonModule } from '@angular/common'; 
 
 @Component({
   selector: 'app-home',
-  imports: [Header, Footer],
+  imports: [Header, Footer, CommonModule],
   templateUrl: './home.html',
   styleUrl: './home.css'
 })
 export class Home {
-
+  filmInEvidenza = [
+    {
+      titolo: 'DEMON SLAYER: Kimetsu no Yaiba',
+      img: '/demonslayer.jpg',
+      info: '2D'
+    },
+    {
+      titolo: 'Troppo Cattivi 2',
+      img: '/troppocattivi2.jpg',
+      info: '3D'
+    },
+    {
+      titolo: 'Dangerous Animals',
+      img: '/dangerousanimals.jpg',
+      info: '2D'
+    }
+  ];
 }
