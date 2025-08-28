e41656f319d26ed89bc456e879dcf1813b472181
Pasquale Russo <pasquale01.russo@edu.unife.it>
2025-08-28 18:15:56 +0200

    aggiunta navbar minimal
    aggiunta pagina registrazione (da completare)

--- Frontend/src/app/header/header.ts
+++ Frontend/src/app/header/header.ts
@@ -1,12 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-header',
-  imports: [RouterModule],
+  imports: [RouterModule, CommonModule],
   templateUrl: './header.html',
   styleUrl: './header.css'
 })
 export class Header {
+ showSignInMenu = false;
+
+  toggleSignInMenu(event: MouseEvent) {
+    event.stopPropagation();
+    this.showSignInMenu = !this.showSignInMenu;
+  }
+
+  @HostListener('document:click')
+  onDocumentClick() {
+    this.showSignInMenu = false;
+  }
+
+  @HostListener('document:keydown.escape')
+  onEsc() {
+    this.showSignInMenu = false;
+  }
 
 }
